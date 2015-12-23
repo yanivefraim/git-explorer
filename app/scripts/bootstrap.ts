87dd8dddbd08f7f43e8e8b7cd089356f3ed1129a
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-12-23 11:02:42 +0200

    bootstrap using UpgradeAdapter

--- app/scripts/bootstrap.ts
+++ app/scripts/bootstrap.ts
@@ -3,9 +3,5 @@ declare var angular: any;
 import {UpgradeAdapter} from 'angular2/upgrade';
 
 let adapter = new UpgradeAdapter();
-console.log(adapter);
-angular.element(document).ready(function() {
-  //console.log('UpgradeAdapter', UpgradeAdapter);
-  angular.bootstrap(document, ['githubExplorerApp']);
-});
 
+adapter.bootstrap(document.body, ['githubExplorerApp']);
