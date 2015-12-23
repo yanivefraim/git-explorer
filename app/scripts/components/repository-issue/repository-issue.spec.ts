bafae1dcbb65017e4bc2da68ca6a77122e0c4a48
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-12-23 12:34:24 +0200

    adding my first ng2 empty component + integrating it into our ng1 app

---
+++ app/scripts/components/repository-issue/repository-issue.spec.ts
@@ -0,0 +1,27 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {RepositoryIssue} from './repository-issue';
+
+
+describe('RepositoryIssue Component', () => {
+
+  beforeEachProviders(() => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(RepositoryIssue).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
---
+++ app/scripts/components/repository-issue/repository-issue.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'repository-issue',
+  template: `<div>123</div>`,
+  //styleUrls: ['app/components/repository-issue/repository-issue.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export default class RepositoryIssue {
+
+  constructor() {}
+
+}
