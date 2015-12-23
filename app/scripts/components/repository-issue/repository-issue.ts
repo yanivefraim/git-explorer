42dbcd1f8e84f9b3dae5e8115ae48ba844164da9
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-12-23 13:47:22 +0200

    changing 'repository-issue' to be an ng2 component

--- app/scripts/components/repository-issue/repository-issue.ts
+++ app/scripts/components/repository-issue/repository-issue.ts
@@ -3,8 +3,21 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'repository-issue',
-  template: `<div>123</div>`,
-  //styleUrls: ['app/components/repository-issue/repository-issue.css'],
+  inputs: ['issue', 'comments'],
+  template: `
+    <div class="panel panel-primary">
+      <div class="panel-heading">issue: #{{issue.number}}</div>
+      <div class="panel-body">
+        <p>{{issue.body}}</p>
+      </div>
+    </div>
+    Comments:
+    <ul class="list-group">
+      <li class="list-group-item" ng-repeat="comment in comments">
+      {{comment.body}}
+      </li>
+    </ul>
+  `,
   providers: [],
   directives: [],
   pipes: []
