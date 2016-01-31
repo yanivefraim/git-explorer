b5e803e9ef3db30573014695b8f5d2289b0990e7
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-01-31 18:07:59 +0200

    Yay! first downgraded ng2 component

--- app/scripts/bootstrap.ts
+++ app/scripts/bootstrap.ts
@@ -1,7 +1,12 @@
 declare var angular: any;
 
 import {UpgradeAdapter} from 'angular2/upgrade';
+import RepositoryIssue from "./components/repository-issue/repository-issue";
 
 let adapter = new UpgradeAdapter();
+
+angular.module('githubExplorerApp').directive('repositoryIssue', adapter.downgradeNg2Component(RepositoryIssue));
+
+adapter.upgradeNg1Provider('dataService');
 
 adapter.bootstrap(document.body, ['githubExplorerApp']);
