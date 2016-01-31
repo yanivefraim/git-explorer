9268b4c2d969b62f86905e23c744da89d363f393
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-01-31 19:06:32 +0200

    refactor repository-issue

--- app/scripts/controllers/repository-issue.js
+++ app/scripts/controllers/repository-issue.js
@@ -8,17 +8,15 @@
  * Controller of the githubExplorerApp
  */
 angular.module('githubExplorerApp')
-  .controller('RepositoryIssueCtrl', function ($stateParams, $scope, issue, dataService) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RepositoryIssueCtrl', function ($stateParams, $scope, issue, repository, dataService) {
 
     console.log('issue.number', issue.number);
     $scope.issue = issue;
 
-    dataService.getIssueComment($stateParams.login, $stateParams.fullName, issue.number).then(function(response) {
+    var login = repository.full_name.split('/')[0];
+    var fullName = repository.full_name.split('/')[1];
+
+    dataService.getIssueComment(login, fullName, issue.number).then(function(response) {
       $scope.comments = response.data;
     });
   });
